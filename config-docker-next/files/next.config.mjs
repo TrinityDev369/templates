71a0960b53f39dabe0a1750811a3c3b4b3791dd0
Sergej Götz <info@trinitystudio.net>
2026-02-18 22:46:40 +0100

    feat: publish template `config-docker-next`

---
+++ config-docker-next/files/next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+
+  // Uncomment and configure to allow external images:
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'cdn.example.com',
+  //       pathname: '/images/**',
+  //     },
+  //   ],
+  // },
+};
+
+export default nextConfig;
